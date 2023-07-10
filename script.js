4a83ff69fb51ab6d8b8e1944a4dafd40eef52589
othmanebe <othmane.bellous@gmail.com>
2023-07-10 14:20:29 +0100

    code optimization

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const importantTasksCount = document.querySelector(".important-tasks");
 const searchBtn = document.querySelector(".search-btn");
 const searchInput = document.querySelector(".search-input");
 let tasksArray =[];
-let filteredArray = [];
+
 
 /*----------------------------------------------------Username-----------------------------------------------------*/
 
@@ -42,32 +42,29 @@ tabs.forEach((tab, index) =>{
         tab.classList.add("active");
         heading.textContent = tab.querySelector(".tab-title").textContent;
         activeTabTitle = tab.querySelector(".tab-title");
-        displayTabContent(activeTabTitle);
+        displayTabContent();
     })
 });
 
-function displayTabContent(tabTitle){
-    if (searchInput.value.trim()) {
-       filteredArray = tasksArray.filter(task => task.content.toLocaleLowerCase().includes(searchInput.value.trim().toLocaleLowerCase()));
-        if(tabTitle.textContent == "My Day"){
+function displayTabContent(){
+       let filteredArray = tasksArray.filter(task => task.content.toLocaleLowerCase().includes(searchInput.value.trim().toLocaleLowerCase()));
+       if (filteredArray.length !== 0) {
+        if(activeTabTitle.textContent == "My Day"){
             addTasksToPage(filteredArray);
-        }else if(tabTitle.textContent == "Important"){
+        }else if(activeTabTitle.textContent == "Important"){
             addTasksToPage(filteredArray.filter(task => task.important));
         }
-    }else{
-        if(tabTitle.textContent == "My Day"){
-            addTasksToPage(tasksArray);
-        }else if(tabTitle.textContent == "Important"){
-            addTasksToPage(tasksArray.filter(task => task.important));
+       }else{
+        if (!document.querySelector(".message")) {
+            addTasksToPage(filteredArray);
+            noTasksMessage();
         }
-    }
+       }
+        
 };
-
-//pass the title of the active tab to heading on page load
 heading.textContent = activeTabTitle.textContent;
 
 /*--------------------------------------------------------Date------------------------------------------------------*/
-//Display date
 let currentDate = new Date();
 const days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
@@ -83,18 +80,15 @@ searchBtn.addEventListener("click", ()=>{
 });
 
 searchInput.addEventListener("input", ()=>{
-    displayTabContent(activeTabTitle);
+    displayTabContent();
 });
 
 /*--------------------------------------------------------Tasks-----------------------------------------------------*/
 
-if(localStorage.getItem("tasks")){
-    if (JSON.parse(localStorage.getItem('tasks')).length !== 0) {
+if(localStorage.getItem("tasks") && JSON.parse(localStorage.getItem('tasks')).length !== 0){
         tasksArray = JSON.parse(localStorage.getItem("tasks")); 
     addTasksToPage(tasksArray);
-    //Update the current number of tasks 
     dailyTasksCount.textContent = tasksArray.length;
-    //Update important tasks
     importantTasksCount.textContent = tasksArray.filter(task => task.important).length;
 
     }else{
@@ -102,16 +96,10 @@ if(localStorage.getItem("tasks")){
         importantTasksCount.textContent = tasksArray.filter(task => task.important).length;
         noTasksMessage();//Show "You have no tasks today!"
     }
-    
-}else{
-    dailyTasksCount.textContent = tasksArray.length;
-    importantTasksCount.textContent = tasksArray.filter(task => task.important).length;
-    noTasksMessage();
-}
 
 function noTasksMessage(){
     let paragraph = document.createElement("p");
-    paragraph.textContent = "You have no tasks today!";
+    paragraph.textContent = "No tasks to show!";
     paragraph.classList.add("message", "text-center", "text-secondary", "mt-5");
     tasksHolder.appendChild(paragraph);
 }
@@ -132,7 +120,7 @@ function addTaskToArray(taskValue) {
         done: false,
     }
     tasksArray.unshift(task); 
-    displayTabContent(activeTabTitle);
+    displayTabContent();
     addTasksToLocalStorage(tasksArray);
     //Update the current number of tasks
     dailyTasksCount.textContent = tasksArray.length;
@@ -141,42 +129,72 @@ function addTaskToArray(taskValue) {
 function addTasksToLocalStorage(array) {
     localStorage.setItem("tasks", JSON.stringify(array)); 
 }
+function createTaskDiv(taskId, taskContent, isImportant, isDone){
+    let taskDiv = document.createElement("div");
+        taskDiv.setAttribute("data-id", taskId);
+        taskDiv.classList.add("task", "d-flex", "align-items-center", "gap-2", "border-bottom", "mx-4");
+        createContentDiv(taskDiv,taskContent, isDone);
+        createDeleteBtn(taskDiv);
+        createFavoriteBtn(taskDiv, isImportant);
+        tasksHolder.appendChild(taskDiv);
+}
+
+function createContentDiv(taskDiv, taskContent, isDone) {
+    let contentDiv = document.createElement("div");
+        contentDiv.classList.add("task-content", "d-flex", "align-items-center", "gap-2", "flex-grow-1", "text-nowrap", "overflow-hidden", "py-2");
+        taskDiv.appendChild(contentDiv);
+        createCheckIcon(contentDiv);
+        createTaskText(contentDiv, taskContent);
+        styleTaskOnClick(contentDiv);
+        addStyleToDoneTasks(isDone, contentDiv);
+}
+
+function createCheckIcon(contentDiv) {
+    let checkIcon = document.createElement("i");
+        checkIcon.classList.add("bi", "bi-circle", "fs-4");
+        contentDiv.appendChild(checkIcon);
+}
+
+function createTaskText(contentDiv, taskContent) {
+    let taskText = document.createElement("span");
+        taskText.classList.add("task-text");
+        taskText.textContent = taskContent; //Pass the value of the content property to the taskText element
+        contentDiv.appendChild(taskText);
+}
+
+function createDeleteBtn(taskDiv) {
+    let delBtn = document.createElement("button");
+    delBtn.classList.add("delete-btn", "text-danger", "bg-transparent", "border-0");
+    taskDiv.appendChild(delBtn);
+    createDeleteIcon(delBtn);
+    deleteTask(delBtn);
+}
+
+function createDeleteIcon(delBtn){
+    let delIcon = document.createElement("i");
+        delIcon.classList.add("bi", "bi-trash", "fs-4");
+        delBtn.appendChild(delIcon);
+}
+
+function createFavoriteBtn(taskDiv, isImportant){
+    let favBtn = document.createElement("button");
+        favBtn.classList.add("favorite-btn", "bg-transparent", "border-0");
+        taskDiv.appendChild(favBtn);
+        createFavoriteIcon(favBtn, isImportant);
+        addTaskToImportant(favBtn);
+}
+
+function createFavoriteIcon(favBtn, isImportant){
+    let favIcon = document.createElement("i");
+        favIcon.classList.add("bi", "bi-star", "fs-4");
+        favBtn.appendChild(favIcon);
+        addFilledStarToImportantTasks(isImportant, favIcon);
+}
 
 function addTasksToPage(array) {
     tasksHolder.innerHTML = "";
     array.forEach(task =>{
-        let taskDiv = document.createElement("div");
-        taskDiv.setAttribute("data-id", task.id); //Create an attribute with a unique value for each task
-        taskDiv.classList.add("task", "d-flex", "align-items-center", "gap-2", "border-bottom", "mx-4");
-        let contentDiv = document.createElement("div");
-        contentDiv.classList.add("task-content", "d-flex", "align-items-center", "gap-2", "flex-grow-1", "text-nowrap", "overflow-hidden", "py-2");
-        let checkIcon = document.createElement("i");
-        checkIcon.classList.add("bi", "bi-circle", "fs-4");
-        contentDiv.appendChild(checkIcon);
-        let taskText = document.createElement("span");
-        taskText.classList.add("task-text");
-        taskText.textContent = task.content; //Pass the value of the content property to the taskText element
-        contentDiv.appendChild(taskText);
-        taskDiv.appendChild(contentDiv);
-        let delBtn = document.createElement("button");
-        delBtn.classList.add("delete-btn", "text-danger", "bg-transparent", "border-0");
-        let delIcon = document.createElement("i");
-        delIcon.classList.add("bi", "bi-trash", "fs-4");
-        delBtn.appendChild(delIcon);
-        taskDiv.appendChild(delBtn);
-        let favBtn = document.createElement("button");
-        favBtn.classList.add("favorite-btn", "bg-transparent", "border-0");
-        let favIcon = document.createElement("i");
-        favIcon.classList.add("bi", "bi-star", "fs-4");
-        favBtn.appendChild(favIcon);
-        taskDiv.appendChild(favBtn);
-        tasksHolder.appendChild(taskDiv);
-
-        deleteTask(delBtn);
-        styleTaskOnClick(contentDiv);
-        addTaskToImportant(favBtn);
-        addFilledStarToImportantTasks(task.important, favIcon);
-        addStyleToDoneTasks(task.done, contentDiv);
+        createTaskDiv(task.id, task.content, task.important, task.done);
     })
 }
 
@@ -184,9 +202,6 @@ function deleteTask(delBtn){
     delBtn.addEventListener("click", ()=>{
         if (confirm("Are you sure you want to delete thi task?")) {
             removeTaskFromLocalStorage(delBtn.parentElement.getAttribute("data-id"));
-            if(tasksArray.length === 0){
-                noTasksMessage();
-            }
         }
     });
 }
@@ -194,8 +209,7 @@ function deleteTask(delBtn){
 function removeTaskFromLocalStorage(id) {
     tasksArray = tasksArray.filter(task => task.id != id); 
     addTasksToLocalStorage(tasksArray);//update local storage
-    displayTabContent(activeTabTitle);
-    //Update the current number of tasks
+    displayTabContent();
     dailyTasksCount.textContent = tasksArray.length;
     importantTasksCount.textContent = tasksArray.filter(task => task.important).length;
 }
@@ -225,7 +239,7 @@ function addTaskToImportant(favBtn){
         })
         addTasksToLocalStorage(tasksArray);
         importantTasksCount.textContent = tasksArray.filter(task => task.important).length;
-        displayTabContent(activeTabTitle);
+        displayTabContent();
     })
 }
 
